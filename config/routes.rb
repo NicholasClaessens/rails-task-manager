b2797f41c04c2f0fa473865e1b1e86ecc35c896c
Nicholas Claessens <nicholas04claessens@yahoo.com>
2020-11-17 13:35:59 +0100

    Exo2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+
+  resources :tasks
+  # # Create
+  # get 'tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+
+  # # Show
+  # get 'tasks/:id', to: 'tasks#show'
+
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete
+  get 'tasks/:id/destroy', to: 'tasks#destroy'
 end
